ed243db78953b999643e96d1c7120b6dc1f965ef
vogiabao123 <146648388+vogiabao123@users.noreply.github.com>
2024-05-16 10:07:35 +0700

    upp

--- DACS Web Viec Lam/Areas/Admin/Controllers/CompanyController.cs
+++ DACS Web Viec Lam/Areas/Admin/Controllers/CompanyController.cs
@@ -38,6 +38,37 @@ namespace DACS_Web_Viec_Lam.Areas.Admin.Controllers
 
             return View(await allEmployee.ToListAsync());
         }
+        public async Task<IActionResult> Edit(string id)
+        {
+            var jobseekers = await _companyRepository.GetByIdAsync(id);
+            if (jobseekers == null)
+            {
+                return NotFound();
+            }
+            return View(jobseekers);
+        }
+
+        // POST: Updates 
+        [HttpPost]
+        public async Task<IActionResult> Edit(string id, ApplicationUser jobseekers)
+        {
+            if (id != jobseekers.Id)
+            {
+                return NotFound();
+            }
+            if (ModelState.IsValid)
+            {
+                var existingJobseeker = await _companyRepository.GetByIdAsync(id);// Giả định có phương thức GetByIdAsync
+                existingJobseeker.FullName = jobseekers.FullName;
+                existingJobseeker.UserName = jobseekers.UserName;
+                // existingJobseeker.PasswordHash = jobseekers.PasswordHash;
+                existingJobseeker.Email = jobseekers.Email;
+                await _companyRepository.UpdateAsync(existingJobseeker);
+                return RedirectToAction(nameof(Index));
+
+            }
+            return View(jobseekers);
+        }
         public async Task<IActionResult> LockAccount(string id)
         {
             var user = await _userManager.FindByIdAsync(id);
