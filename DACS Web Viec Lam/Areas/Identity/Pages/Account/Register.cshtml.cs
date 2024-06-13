fb0a11a61d6c35ef3e336fe1cf44d49d7a2ddaf8
vogiabao123 <146648388+vogiabao123@users.noreply.github.com>
2024-06-14 00:11:22 +0700

    a

--- DACS Web Viec Lam/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ DACS Web Viec Lam/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -192,7 +192,7 @@ namespace DACS_Web_Viec_Lam.Areas.Identity.Pages.Account
 
                 if (result.Succeeded)
                 {
-                    TempData["SuccessMessage"] = "Registeted Succesfully";
+                    TempData["SuccessMessage"] = "Registered Successfully";
                     _logger.LogInformation("User created a new account with password.");
 
                     var userId = await _userManager.GetUserIdAsync(user);
@@ -201,34 +201,29 @@ namespace DACS_Web_Viec_Lam.Areas.Identity.Pages.Account
                     var callbackUrl = Url.Page(
                         "/Account/ConfirmEmail",
                         pageHandler: null,
-values: new { area = "Identity", userId = userId, code = code, returnUrl = returnUrl },
+                        values: new { area = "Identity", userId = userId, code = code, returnUrl = returnUrl },
                         protocol: Request.Scheme);
 
                     await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
                         $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
 
-                    //if (_userManager.Options.SignIn.RequireConfirmedAccount)
-                    //{
-                    //    return RedirectToPage("RegisterConfirmation", new { email = Input.Email, returnUrl = returnUrl });
-                    //}
-                    //else
-                    //{
-                    //    await _signInManager.SignInAsync(user, isPersistent: false);
-                    //    return LocalRedirect(returnUrl);
-                    //}
+                    // Chuyển hướng đến trang đăng nhập sau khi đăng ký thành công
+                    return RedirectToPage("/Account/Login", new { area = "Identity" });
                 }
                 foreach (var error in result.Errors)
                 {
                     ModelState.AddModelError(string.Empty, error.Description);
                 }
             }
+
+            // Thiết lập lại role list nếu có lỗi
             Input.RoleList = _roleManager.Roles.Select(x => x.Name).Select(i => new SelectListItem
             {
                 Text = i,
                 Value = i
             });
 
-            // If we got this far, something failed, redisplay form
+            // Nếu có lỗi, hiển thị lại form đăng ký
             return Page();
         }
 
